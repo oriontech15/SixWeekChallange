5d39b326749afe6e4dcc630b5a4701cdd5a689f3
Justin Smith <11jsmith7@gmail.com>
2015-08-12 10:14:03 -0600

    Initial Commit

---
+++ PairsGenerator/PairsGenerator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PairsGenerator
+//
+//  Created by Justin Smith on 8/12/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PairsGenerator/PairsGenerator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PairsGenerator
+//
+//  Created by Justin Smith on 8/12/15.
+//  Copyright (c) 2015 Justin Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
